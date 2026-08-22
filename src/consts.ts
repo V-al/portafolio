feb812234901848053e2c56f222dc44249990f0d
Val <124918918+Vaaal0@users.noreply.github.com>
2026-08-21 21:32:21 -0600

    paginas

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Val Aguilera';
-export const SITE_DESCRIPTION = 'Artista Digital';
+export const SITE_TITLE = 'Portafolio';
+export const SITE_DESCRIPTION = 'Valeria Aguilera | Artista Digital';
